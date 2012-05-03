ab2c67c204123b4e4eb9649c39912977c1b70ed4
Max Maischein <corion@corion.net>
2012-05-03 22:56:45 +0200

    Add the fake demo app

---
+++ lib/Dancer/Layout/Bootstrap.pm
@@ -0,0 +1,69 @@
+package Dancer::Layout::Bootstrap;
+use Dancer ':syntax';
+use Dancer::Plugin::FlashMessage;
+
+=head1 NAME
+
+Dancer::Layout::Bootstrap
+
+=head1 ABSTRACT
+
+Template and dummy application that applies the
+Bootstrap CSS to Dancer.
+
+=head1 SUPPORTED FEATURES
+
+If you want to show alerts etc., the main
+template already provides support for that
+via L<Dancer::Plugin::FlashMessage>.
+
+    flash success => sprintf "Welcome back, %s",
+        session('user')->{name};
+
+... will produce a nice, green "Welcome back"
+message for the user.
+
+All flash message output is HTML escaped for security reasons.
+
+=head1 NEEDED MODULES
+
+The simple templating included with Dancer does not properly
+support user log-in / log out, so the template needs
+L<Template::Toolkit>.
+
+=head1 SEE ALSO
+
+L<http://twitter.github.com/bootstrap/> - Bootstrap
+
+L<http://glyphicons.com/> - Icons from Glyphicons Free, licensed under CC BY 3.0.
+
+L<http://jquery.com/> - jQuery 1.7.2
+
+=cut
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+get '/user/login' => sub {
+    session user => { name => 'Bob' };
+
+    flash success => sprintf "Welcome back, %s",
+        session('user')->{name};
+    
+    redirect '/';
+};
+
+get '/user/logout' => sub {
+    my $user= session('user')->{name};
+    session user => undef;
+
+    flash success => sprintf "Goodbye, %s",
+        $user;
+    
+    redirect '/';
+};
+
+true;
